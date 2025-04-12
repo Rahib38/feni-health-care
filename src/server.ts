82e5d42a52fa5c8bd448358a7349b696563ce882
Nadimul Rahib <nadimulrahib38@gmail.com>
2025-04-12 14:18:09 -0700

    project setup update

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,11 @@
-import express from 'express'
-const app = express()
+import { Server } from "http";
+import app from "./app";
 
-const port = 3001
+const port = 3001;
 
-app.listen(port,()=>{
-    console.log("App is listening on port", port)
-})
+async function main() {
+  const server: Server = app.listen(port, () => {
+    console.log("Server is running on port", port);
+  });
+}
+main()
