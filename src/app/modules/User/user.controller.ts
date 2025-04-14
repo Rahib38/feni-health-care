154cc76198b200b8e11735e3daf9d1f0033c7cee
Nadimul Rahib <nadimulrahib38@gmail.com>
2025-04-13 21:16:12 -0700

    error handleing

--- src/app/modules/User/user.controller.ts
+++ src/app/modules/User/user.controller.ts
@@ -2,8 +2,21 @@ import { Request, Response } from "express";
 import { userService } from "./user.service";
 
 const createAdmin = async (req:Request, res:Response)=>{
-    const result = await userService.createAdmin(req.body)
-    res.send(result)
+    try{
+        const result = await userService.createAdmin(req.body)
+        // res.send(result)
+        res.status(200).json({
+            success: true,
+            message: "Admin Created successfuly..!",
+            data: result
+        })
+    }catch(err){
+        res.status(500).json({
+            success:false,
+            message:err?.name || "Something went wrong!",
+            error: err
+        })
+    }
     // console.log(req.body)
 }
 export const userController = {
