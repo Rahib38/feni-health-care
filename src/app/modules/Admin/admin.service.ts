f421281dd839a735dbef97b3284a0ba8e3a0660d
Nadimul Rahib <nadimulrahib38@gmail.com>
2025-04-14 00:17:40 -0700

    search optimize

--- src/app/modules/Admin/admin.service.ts
+++ src/app/modules/Admin/admin.service.ts
@@ -1,29 +1,32 @@
-import { PrismaClient } from "../../../generated/prisma";
+import { Prisma, PrismaClient } from "../../../generated/prisma";
 
-const prisma = new PrismaClient()
-const getAdminAllFromDB = async(params:any)=>{
-    console.log({params})
-    const result = await prisma.admin.findMany({
-        where:{
-           OR:[
-            {
-                name:{
-                    contains: params.searchTerm,
-                    mode:"insensitive"
-                }
-            },
-            {
-                email:{
-                    contains: params.searchTerm,
-                    mode:"insensitive"
-                }
-            }
-           ] 
-        }
+const prisma = new PrismaClient();
+const getAdminAllFromDB = async (params: any) => {
+  const andCondions: Prisma.AdminWhereInput[] = [];
+
+  const adminSearchAbleFields=["name", "email"]
+ 
+
+  if (params.searchTerm) {
+    andCondions.push({
+      OR: adminSearchAbleFields.map((field) => ({
+        [field]: {
+          contains: params.searchTerm,
+          mode: "insensitive",
+        },
+      })),
     });
-    return result
-}
+  }
 
-export const AdminService={
-    getAdminAllFromDB
-}
+  // console.dir(andCondions,{depth:"inifinity"})
+
+  const whereConditions: Prisma.AdminWhereInput = { AND: andCondions };
+  const result = await prisma.admin.findMany({
+    where: whereConditions,
+  });
+  return result;
+};
+
+export const AdminService = {
+  getAdminAllFromDB,
+};
